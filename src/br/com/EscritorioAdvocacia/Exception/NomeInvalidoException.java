b89631d4807a23919519a53d31e44ee19d1468ed
pedromeireless <pedrohcmeirelles@gmail.com>
2022-07-14 11:23:18 -0300

    create exception

---
+++ src/br/com/EscritorioAdvocacia/Exception/NomeInvalidoException.java
@@ -0,0 +1,9 @@
+package br.com.EscritorioAdvocacia.Exception;
+
+public class NomeInvalidoException extends RuntimeException{
+    
+    public NomeInvalidoException(String msg){
+        super(msg);
+    }
+
+}
