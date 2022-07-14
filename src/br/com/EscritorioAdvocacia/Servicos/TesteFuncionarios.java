442542205d095dab042a86ce081091d58a39901f
pedromeireless <pedrohcmeirelles@gmail.com>
2022-07-14 11:26:19 -0300

    remove tests

--- src/br/com/EscritorioAdvocacia/Servicos/TesteFuncionarios.java
+++ src/br/com/EscritorioAdvocacia/Servicos/TesteFuncionarios.java
@@ -5,10 +5,6 @@ import br.com.EscritorioAdvocacia.Modelos.*;
 public class TesteFuncionarios {
 
 	public static void main(String[] args) {
-		Funcionario joao = new Estagiario("Joao", 1000, "Estagiário");
-		Funcionario pedro = new Assessor("Pedro", 1600, "Assessor");
-		Funcionario marcelo = new Advogado("Marcelo", 2000, "Advogado");
-		Gerente carlos = new Gerente("Carlos", 3000, "Gerente", 2345);
 		Socio ruan = new Socio("Ruan", 4000, "Sócio", 2345);
 			
 		SistemaInterno si = new SistemaInterno();
